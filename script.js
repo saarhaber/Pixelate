ea6dcf5b5c66d9251a97cf7ef7a259a9aaaa457e
saarhaber <saarhaber@gmail.com>
2019-07-19 17:31:17 -0400

    checking

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 function colorAll(myColor) {
-   var row = document.getElementsByClassName("tdclass");
+   var row = document.getElementsByClassName("column");
    for (let i=0; i<row.length; i++)
     row[i].style.backgroundColor = myColor;
 }
 
-let myColor;
+let myColor = "grey";
 function colorCollector()
 {
     myColor = document.getElementsByClassName("colorDropdown")[0].value;
+}
 
+function colorRemain(s) {
+    let row = document.getElementsByClassName("column")[0];
+    for (let i=0; i<row.length; i++)
+    { let element = row[i];
+        if (element.style.backgroundColor == "grey") {
+            element.style.backgroundColor = myColor;
+        }
+    }
+}
 
+function colorReset() {
+    let row = document.getElementsByClassName("column");
+    for (let i=0; i<row.length; i++)
+    { let element = row[i];
+      element.style.backgroundColor = "grey";
+    } 
 }
