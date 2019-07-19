f90f1fef920ba32b9753ddf4702d5dd35c598f65
Mahdiur Rahman <rmahdiur@gmail.com>
2019-07-19 14:05:06 -0400

    addrow functionalities

---
+++ addRow.js
@@ -0,0 +1,19 @@
+let addRowButton = document.querySelector(".addRowButton")
+
+let addRowFunction = () => {
+    let tbody = document.querySelector(".mainGrid tbody")
+    console.log(tbody);
+    let tr = document.querySelector(".row")
+    let tr_html = "<tr class=\"row\">" + tr.innerHTML + "</tr>"
+    tbody.innerHTML += tr_html
+    
+    let td_list = document.querySelectorAll(".mainGrid tr:last-child td")
+    for (let index = 0; index < td_list.length; index++) {
+        td_list[index].style.backgroundColor = "grey"
+    }
+}
+
+addRowButton.onclick = function(event) {
+    console.log("clicked addRowButton")
+    addRowFunction()
+}
