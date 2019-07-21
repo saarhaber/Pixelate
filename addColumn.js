4b7249fd5911c58953e07bbd3735ced9b5fb5062
NapatKarm <napat.karm@gmail.com>
2019-07-21 13:01:48 -0400

    Added on to AddCol function to add a column when there are no rows on screen

--- addColumn.js
+++ addColumn.js
@@ -3,23 +3,37 @@ let addColumnButton = document.querySelector(".addColumnButton")
 addColumnButton.addEventListener('click', function(event)
 {
 	let allrows = document.getElementsByClassName("row");
-	for(let step = 0; step < allrows.length; step++)
+	if(allrows.length == 0)
 	{
-        let newcol = document.createElement("td");
+		let table = document.getElementsByClassName("mainGrid");
+	    let newrow = document.createElement("tr");
+		newrow.setAttribute('class', 'row');
+		table[0].appendChild(newrow);
+		let newcol = document.createElement("td");
 		newcol.setAttribute('class', 'column');
 		newcol.style.backgroundColor = "grey";
-		// newcol.addEventListener('mousedown', function(event) {
-		// 	event.preventDefault()
-		// 	startDrawing(newcol)
-		// })
-		// newcol.addEventListener('mousemove', function(event) {
-		// 	event.preventDefault()
-		// 	whileDrawing(newcol)
-		// })
-		// newcol.addEventListener('mouseup', function(event) {
-		// 	event.preventDefault()
-		// 	finishDrawing(newcol)
-		// })
-		allrows[step].appendChild(newcol);
+		newrow.appendChild(newcol);	
+	}
+	else
+	{
+		for(let step = 0; step < allrows.length; step++)
+		{
+	        let newcol = document.createElement("td");
+			newcol.setAttribute('class', 'column');
+			newcol.style.backgroundColor = "grey";
+			// newcol.addEventListener('mousedown', function(event) {
+			// 	event.preventDefault()
+			// 	startDrawing(newcol)
+			// })
+			// newcol.addEventListener('mousemove', function(event) {
+			// 	event.preventDefault()
+			// 	whileDrawing(newcol)
+			// })
+			// newcol.addEventListener('mouseup', function(event) {
+			// 	event.preventDefault()
+			// 	finishDrawing(newcol)
+			// })
+			allrows[step].appendChild(newcol);
+		}
 	}
 })
