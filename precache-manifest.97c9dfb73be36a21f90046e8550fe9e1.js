094588503d9f6d90786a4c4a9533eaab0b9a4869
NapatKarm <napat.karm@gmail.com>
2019-07-29 14:32:41 -0400

    Updates

---
+++ precache-manifest.97c9dfb73be36a21f90046e8550fe9e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23552037e00c7ea8dc4dfd67399da7a9",
+    "url": "/Pixelate-React/index.html"
+  },
+  {
+    "revision": "a5799c974a3b9849279d",
+    "url": "/Pixelate-React/static/css/main.cebb4f78.chunk.css"
+  },
+  {
+    "revision": "36124575841ff54c6dea",
+    "url": "/Pixelate-React/static/js/2.86c0466d.chunk.js"
+  },
+  {
+    "revision": "a5799c974a3b9849279d",
+    "url": "/Pixelate-React/static/js/main.dd57c669.chunk.js"
+  },
+  {
+    "revision": "2f621b00320938f00e54",
+    "url": "/Pixelate-React/static/js/runtime~main.507451a3.js"
+  }
+]);
